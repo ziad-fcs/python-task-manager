ca383063053cab340deedace6a01806d5815247d
ziad-fcs <daiznarmo@gmail.com>
2025-06-20 17:02:40 +0300

    creating students & validating their grades

--- students-grade-analyzer.py
+++ students-grade-analyzer.py
@@ -1,13 +1,25 @@
+#Function that creates a student
+def create_student(name,grade): 
+    if grade.isdigit():
+        grade = int(grade)
+        if grade >=0 and grade <=100:
+            return [name, grade]
+        else: print("Grade must be between 1 and 100")
+    else: print("enter a valid number")
+
 student_number = input("How many students do you have? ")
-if student_number.isdigit():
+if student_number.isdigit() and int(student_number) > 0:
     student_number=int(student_number)
     students = []
-
 else:
     print("Enter a valid number")
     exit()
 
-name = input("Student name: ")
-grade = input("Student grade: ")
+#Creating multiple students
+for i in range(student_number):
+    name = input("Name: ")
+    grade = input("Grade: ")
+    student = create_student(name,grade)
+    students.append(student)
 
-#checking if grade is int & between 1 and 100
+print(students)
