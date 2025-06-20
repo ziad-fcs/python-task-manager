cc680959c3c65004f920fa21e57ea5b519a21f2f
ziad-fcs <daiznarmo@gmail.com>
2025-06-20 13:37:07 +0300

    Taking the number of students

--- students-grade-analyzer.py
+++ students-grade-analyzer.py
@@ -1,10 +1,13 @@
-name = input("Student name: ")
+student_number = input("How many students do you have? ")
+if student_number.isdigit():
+    student_number=int(student_number)
+    students = []
 
+else:
+    print("Enter a valid number")
+    exit()
+
+name = input("Student name: ")
 grade = input("Student grade: ")
 
 #checking if grade is int & between 1 and 100
-
-if grade.isdigit():
-    grade=int(grade)
-    if grade>= 1 and grade<= 100:
-        
