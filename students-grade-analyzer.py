b5411814fa1a682ab174d3eee8ab6075d6d2b8ba
ziad-fcs <daiznarmo@gmail.com>
2025-06-20 13:11:15 +0300

    test

---
+++ students-grade-analyzer.py
@@ -0,0 +1,11 @@
+name = input("Student name: ")
+
+grade = input("Student grade: ")
+
+#checking if grade is int & between 1 and 100
+
+# if grade.isdigit():
+#     grade=int(grade)
+#     if grade>= 1 and grade<= 100:
+#         continue
+    
