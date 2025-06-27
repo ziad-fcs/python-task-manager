cbd594b1567f40441afead4d0a364e848d2eee12
ziad-fcs <daiznarmo@gmail.com>
2025-06-27 20:52:23 +0300

    Created insert function

--- python-task-manager.py
+++ python-task-manager.py
@@ -1,2 +1,13 @@
-#Hello
-#test
+def insert(title, duration, priority):
+    task= [title,duration, priority]
+    queue.append(task)
+    
+queue = []
+# #We ask the user to give us the number of tasks
+# task_number = input("Please input the number of tasks: ")
+# #We need to verify that the user's input is a positive number (int)
+
+# if not task_number.isdigit():
+#     print("Not a digit")
+# else: 
+
